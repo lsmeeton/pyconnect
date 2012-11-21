ddef47e8b660bf9812b2aac5caf270b32656d4e5
Lewis Smeeton <lcs137@bham.ac.uk>
2012-11-21 15:02:04 +0000

    proof-of-concept version fork pyconnect working without wxPython, and producing pyplot graphs. NOTE, savefig not yet implemented.

--- PyConnect/source/MatplotlibGUI.py
+++ PyConnect/source/MatplotlibGUI.py
@@ -5,18 +5,18 @@ import wx
 import os
 import matplotlib
 matplotlib.use('WXAgg')
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_wxagg import \
-    FigureCanvasWxAgg as FigCanvas, \
-    NavigationToolbar2WxAgg as NavigationToolbar
+#from matplotlib.figure import Figure
+#from matplotlib.backends.backend_wxagg import \
+#    FigureCanvasWxAgg as FigCanvas, \
+#    NavigationToolbar2WxAgg as NavigationToolbar
 from matplotlib import rc
 #from matplotlib import colors, cm
 #from matplotlib.collections import LineCollection
 #from matplotlib.backends.backend_pdf import PdfPages
-#import numpy as np
-#import matplotlib.pylab as plt
-import matplotlib.lines as lns
-from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import matplotlib.pylab as plt
+#import matplotlib.lines as lns
+#from mpl_toolkits.mplot3d import Axes3D
 from KeywordInit import Keywords
 from DisconnectPlot import DisconnectPlot
 import time
@@ -28,14 +28,14 @@ rc('font', family='serif')
 class DGCanvasFrame():
     def __init__(self,disc):
         
-        super(DGCanvasFrame,self).__init__()
+#        super(DGCanvasFrame,self).__init__()
         
         self.disc = disc
    
         self.init_plot()
                 
     def init_plot(self):
-        self.fig = Figure()
+        self.fig = plt.figure()
 
         self.ax = self.fig.add_subplot(111)
 #        self.colbar = self.fig.add_subplot(122)
@@ -117,15 +117,7 @@ class DGCanvasFrame():
                 p = self.disc.basin_index['Level'][l]['Basin'][b]\
                     ['Parents']
                 self.LinesDG(l,b,c,p)
-#        lines = LineCollection(self.plot_list)#,
-#                               linewidth = 0.2,
-                               #color=self.colour_list)
-        
-#        lines.set_array(self.disc.map.get_array())
-#        self.ax.plot(self.plot_listx,self.plot_listz, c=self.colour_list, linewidth=0.2)
-#        self.ax.add_collection(lines)
-#        print lines.__dict__
-#        print self.ax.__dict__
+
 
     def LinesDG(self,l,b,c,p):
         rgb = self.disc.basin_index['Level'][l]['Basin'][b]['RGB']
@@ -142,34 +134,8 @@ class DGCanvasFrame():
         else:
             z2 = self.disc.basin_index['Level'][l]['Energy']      
         self.plot_dataDG = self.ax.plot([x1,x2],[z1,z2], c=rgb, linewidth=0.2)
-#        self.plot_list.append([(x1,z1),(x2,z2)])
-#        self.plot_list.append([(x1,z1),(x2,z2)])
-#        self.plot_list.append(None)
-#        self.plot_listx.extend([x1,x2])
-#        self.plot_listx.append(None)
-#        self.plot_listz.extend([z1,z2])
-#        self.plot_listz.append(None)
-#        self.colour_list.extend(rgb)
-#        self.colour_list.append(None)
-#    
-    def on_save_plot(self, event):
-        file_choices = "PS (*.ps)|*.ps"
-        
-        dlg = wx.FileDialog(
-            self, 
-            message="Save plot as...",
-            defaultDir=os.getcwd(),
-            defaultFile="plot.ps",
-            wildcard=file_choices,
-            style=wx.SAVE)
-        
-        if dlg.ShowModal() == wx.ID_OK:
-            path = dlg.GetPath()
-            self.canvas.print_figure(path, dpi=self.dpi)
-            self.flash_status_message("Saved to %s" % path)
-    
-    def on_exit(self, event):
-        self.Destroy()
+
+
     
 class MDGCanvasFrame(wx.Frame):
     def __init__(self,disc,Q,parent=None,id=-1):
@@ -483,181 +449,7 @@ to file")
     def on_exit(self, event):
         self.Destroy()
         
-#class CanvasFrame(wx.Frame):
-#    def __init__(self,disc,parent=None,id=-1):
-#        
-#        super(CanvasFrame,self).__init__(parent=None,id=-1)
-#        
-#        self.disc = disc
-#        #self.panel = wx.Panel(self)
-#        #self.fig = Figure()
-#        #self.ax = self.fig.add_subplot(111)
-#        
-#        self.create_menu()
-#        self.create_status_bar()
-#        #    self.add_toolbar()
-#        self.create_main_panel()
-#        
-#                
-#    def init_plot(self):
-#        self.fig = Figure()
-#
-#        self.ax = self.fig.add_subplot(121)
-#        self.ax = self.fig.add_subplot(122, projection='3d')
-#        
-#        self.ax.set_title('Disconnectivity Graph')
-#        self.ax.set_title('Metric Disconnectivity Graph')
-#        
-#        # Turns off axes and ticks for in the top and right hand portions
-#        # of the plot, and sets the bottom and left hand axis back from the
-#        # plot.
-#        for loc, spine in self.ax.spines.iteritems():
-#            if loc in ['left','bottom']:    
-#                spine.set_position(('outward',10)) # outward by 10 points                                                                               
-#            elif loc in ['right','top']:
-#                spine.set_color('none') # don't draw spine                                                                                              
-#            else:
-#                raise ValueError('unknown spine location: %s'%loc)
-#            
-#        self.ax.xaxis.set_ticks_position('bottom')
-#        self.ax.yaxis.set_ticks_position('left')
-#
-#
-#        self.PlotDG()
-#        self.PlotMDG()
-#
-#    def create_menu(self):
-#        self.menubar = wx.MenuBar()
-#        
-#        menu_file = wx.Menu()
-# 
-#    m_expt = menu_file.Append(-1, "&Save plot\tCtrl-S", "Save plot \
-#to file")
-#        self.Bind(wx.EVT_MENU, self.on_save_plot, m_expt)
-#        menu_file.AppendSeparator()
-#        m_exit = menu_file.Append(-1, "E&xit\tCtrl-X", "Exit")
-#        self.Bind(wx.EVT_MENU, self.on_exit, m_exit)
-#
-#        self.menubar.Append(menu_file, "&File")
-#    self.SetMenuBar(self.menubar)
-#
-#
-#    def add_toolbar(self):
-#        self.toolbar = NavigationToolbar(self.canvas)
-#        self.toolbar.Realize()
-#        #if wx.Platform == '__WXMAC__':
-#            # Mac platform (OSX 10.3, MacPython) does not seem to cope with
-#            # having a toolbar in a sizer. This work-around gets the buttons
-#            # back, but at the expense of having the toolbar at the top
-#        #    self.SetToolBar(self.toolbar)
-#        #else:
-#            # On Windows platform, default window size is incorrect, so set
-#            # toolbar width to figure width.
-#            #tw, th = self.toolbar.GetSizeTuple()
-#            #fw, fh = self.canvas.GetSizeTuple()
-#            # By adding toolbar in sizer, we are able to put it at the bottom
-#            # of the frame - so appearance is closer to GTK version.
-#            # As noted above, doesn't work for Mac.
-#            #self.toolbar.SetSize(wx.Size(fw, th))
-#            #self.sizer.Add(self.toolbar, 0, wx.LEFT | wx.EXPAND)
-#        # update the axes menu on the toolbar
-#        self.toolbar.update()
-#
-#       
-#    def create_main_panel(self):
-#        self.panel = wx.Panel(self)
-#
-#        self.init_plot()
-#        self.canvas = FigCanvas(self.panel, -1, self.fig)
-#        self.add_toolbar()
-#        self.ax.mouse_init()        
-#        self.vbox = wx.BoxSizer(wx.VERTICAL)
-#        self.vbox.Add(self.canvas, 1, flag=wx.LEFT | wx.TOP | wx.GROW)        
-#        self.hbox2 = wx.BoxSizer(wx.HORIZONTAL)
-#        self.hbox2.Add(self.toolbar, border=5, flag=wx.ALL)
-#        self.vbox.Add(self.hbox2, 0, flag=wx.ALIGN_LEFT | wx.TOP)
-#        
-#        self.panel.SetSizer(self.vbox)
-#        self.vbox.Fit(self)
-#    
-#    def create_status_bar(self):
-#        self.statusbar = self.CreateStatusBar()
-#
-#        
-#    def PlotDG(self):
-#        for l in self.disc.basin_index['Level']:
-#            if l == 1: continue
-#            for b in self.disc.basin_index['Level'][l]['Basin']:
-#                c = self.disc.basin_index['Level'][l]['Basin'][b]\
-#                    ['Children']
-#                p = self.disc.basin_index['Level'][l]['Basin'][b]\
-#                    ['Parents']
-#                self.LinesDG(l,b,c,p)
-#
-#
-#    def PlotMDG(self):
-#        for l in self.disc.basin_index['Level']:
-#            if l == 1: continue
-#            for b in self.disc.basin_index['Level'][l]['Basin']:
-#                c = self.disc.basin_index['Level'][l]['Basin'][b]\
-#                    ['Children']
-#                p = self.disc.basin_index['Level'][l]['Basin'][b]\
-#                    ['Parents']
-#                self.LinesMDG(l,b,c,p)
-#
-#
-#    def LinesDG(self,l,b,c,p):
-#        rgb = self.disc.basin_index['Level'][l]['Basin'][b]['RGB']
-#
-#        x1 = self.disc.basin_index['Level'][l-1]['Basin'][p]['X']
-#
-#        z1 = 1.0*(self.disc.basin_index['Level'][l-1]['Energy'])
-#        x2 = self.disc.basin_index['Level'][l]['Basin'][b]['X']
-#
-#        if not c:
-#            z2 = self.disc.basin_index['Level'][l]['Basin'][b]['Energy']
-#        else:
-#            z2 = self.disc.basin_index['Level'][l]['Energy']      
-#        self.plot_dataDG = self.ax.plot([x1,x2],[z1,z2], color =rgb, linewidth=0.2)
-#    
-#
-#    def LinesMDG(self,l,b,c,p):
-#        rgb = self.disc.basin_index['Level'][l]['Basin'][b]['RGB']
-#
-#        x1 = self.disc.basin_index['Level'][l-1]['Basin'][p]['GLX']
-#        y1 = self.disc.basin_index['Level'][l-1]['Basin'][p]['GLY']
-#        z1 = 1.0*(self.disc.basin_index['Level'][l-1]['GLZ'])
-#
-#        x2 = self.disc.basin_index['Level'][l]['Basin'][b]['GLX']
-#        y2 = self.disc.basin_index['Level'][l]['Basin'][b]['GLY']
-#        if not c:
-#            z2 = self.disc.basin_index['Level'][l]['Basin'][b]['GLZ']
-#
-#        else:
-#            z2 = self.disc.basin_index['Level'][l]['GLZ']#+0.5          
-#            self.plot_dataMDG = self.ax.plot([x1,x2],[y1,y2],[z1,z2], color =rgb, alpha = 0.75,
-#                                                linewidth=0.2)
-#    
-#    def on_save_plot(self, event):
-#        file_choices = "PS (*.ps)|*.ps"
-#        
-#        dlg = wx.FileDialog(
-#            self, 
-#            message="Save plot as...",
-#            defaultDir=os.getcwd(),
-#            defaultFile="plot.ps",
-#            wildcard=file_choices,
-#            style=wx.SAVE)
-#        
-#        if dlg.ShowModal() == wx.ID_OK:
-#            path = dlg.GetPath()
-#            self.canvas.print_figure(path, dpi=self.dpi)
-#            self.flash_status_message("Saved to %s" % path)
-#    
-#    def on_exit(self, event):
-#        self.Destroy()
 
-    
 
 if __name__ == '__main__':
     t00 = time.time()
@@ -737,46 +529,47 @@ if __name__ == '__main__':
 
     print disc.minima_index['Index'][5]
 
-#    DG = DGCanvasFrame(disc).init_plot()
-#    DG.savefig("tree.eps", formate="eps")
-    app = wx.PySimpleApp()
-    if disc.kw.metric3d['present']:
-        t0 = time.time()
-        app.DGframe = DGCanvasFrame(disc)
-        t1 = time.time()
-        print 'DG %2.6f'%(t1-t0)
-        t0 = time.time()
-        app.MDG1frame = MDGCanvasFrame(disc,Q='X')
-        t1 = time.time()
-        print 'MDG1 %2.6f'%(t1-t0)
-        t0 = time.time()
-        app.MDG2frame = MDGCanvasFrame(disc,Q='Y')
-        t1 = time.time()
-        print 'MDG2 %2.6f'%(t1-t0)
-        t0 = time.time()
-        app.MDG3Dframe = MDG3DCanvasFrame(disc)
-        t1 = time.time()
-        print 'MDG3D %2.6f'%(t1-t0)
-        
-        app.DGframe.Show()
-        app.MDG1frame.Show()
-        app.MDG2frame.Show()
-        app.MDG3Dframe.Show()
-    elif disc.kw.metric['present']:
-        app.DGframe = DGCanvasFrame(disc)
-        app.MDGframe = MDGCanvasFrame(disc,Q='X')
-        
-        app.DGframe.Show()
-        app.MDGframe.Show()
+    DG = DGCanvasFrame(disc)
+    plt.show()
+
+#    app = wx.PySimpleApp()
+#    if disc.kw.metric3d['present']:
+#        t0 = time.time()
+#        app.DGframe = DGCanvasFrame(disc)
+#        t1 = time.time()
+#        print 'DG %2.6f'%(t1-t0)
+#        t0 = time.time()
+#        app.MDG1frame = MDGCanvasFrame(disc,Q='X')
+#        t1 = time.time()
+#        print 'MDG1 %2.6f'%(t1-t0)
+#        t0 = time.time()
+#        app.MDG2frame = MDGCanvasFrame(disc,Q='Y')
+#        t1 = time.time()
+#        print 'MDG2 %2.6f'%(t1-t0)
+#        t0 = time.time()
+#        app.MDG3Dframe = MDG3DCanvasFrame(disc)
+#        t1 = time.time()
+#        print 'MDG3D %2.6f'%(t1-t0)
+#        
+#        app.DGframe.Show()
+#        app.MDG1frame.Show()
+#        app.MDG2frame.Show()
 #        app.MDG3Dframe.Show()
-    else:
-        app.DGframe = DGCanvasFrame(disc)
-        
-        app.DGframe.Show()
-#    app.MDGframe.Show()
-#    app.MDG3Dframe.Show()
-#    app.DGframe.Show()
-#    app.MDGframe.Show()
-#    app.MDG3Dframe.Show()
-    app.MainLoop()
- #   GUI.MainWin(disc)
+#    elif disc.kw.metric['present']:
+#        app.DGframe = DGCanvasFrame(disc)
+#        app.MDGframe = MDGCanvasFrame(disc,Q='X')
+#        
+#        app.DGframe.Show()
+#        app.MDGframe.Show()
+##        app.MDG3Dframe.Show()
+#    else:
+#        app.DGframe = DGCanvasFrame(disc)
+#        
+#        app.DGframe.Show()
+##    app.MDGframe.Show()
+##    app.MDG3Dframe.Show()
+##    app.DGframe.Show()
+##    app.MDGframe.Show()
+##    app.MDG3Dframe.Show()
+#    app.MainLoop()
+# #   GUI.MainWin(disc)
